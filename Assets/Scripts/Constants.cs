d93ae01ad028b4a382b82dacf10bea66f2eb7ce6
Jonas <heschl@protonmail.com>
2021-01-26 15:56:16 +0100

    Initial commit

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants : MonoBehaviour
+{
+    /**
+     * The height of a tile in units. Since all tiles are square, their width equals the this height.
+     */
+    public const float TileHeight = 0.8f;
+}
---
+++ Assets/Scripts/GlobalData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class GlobalData
+{
+    #region SelectedUnit
+    public event EventHandler<(GameObject oldValue, GameObject newValue)> SelectedUnitChanged;
+    
+    /**
+     * The GameObject of the currently selected unit.
+     */
+    [CanBeNull] private GameObject selectedUnit;
+    [CanBeNull] public GameObject SelectedUnit
+    {
+        get => selectedUnit;
+        set
+        {
+            SelectedUnitChanged?.Invoke(null, (selectedUnit, value));
+            selectedUnit = value;
+        }
+    }
+    #endregion SelectedUnit
+
+    #region SingletonBoilerplate
+    private static GlobalData _instance;
+
+    public static GlobalData Instance =>
+        _instance ?? (_instance = new GlobalData());
+    #endregion SingletonBoilerplate
+}
